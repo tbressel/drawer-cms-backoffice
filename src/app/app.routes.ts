fc93dbf4e4aba57da24c69619d991a3187bba646
tbressel <tbressel.dev@gmail.com>
2024-07-14 19:11:23 +0200

    style: fix css style on all article component and merge with create commponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,7 +8,6 @@ import { LogoutComponent } from './backoffice/logout/logout.component';
 import { HomeComponent } from './backoffice/home/home.component';
 import { MenuSettingComponent } from './backoffice/menu-setting/menu-setting.component';
 import { AllArticlesComponent } from './backoffice/all-articles/all-articles.component';
-import { CreateArticleComponent } from './backoffice/create-article/create-article.component';
 import { EditArticleComponent } from './backoffice/edit-article/edit-article.component';
 import { DiskUnitsSettingComponent } from './backoffice/disk-units-setting/disk-units-setting.component';
 import { UnitFilesSettingComponent } from './backoffice/unit-files-setting/unit-files-setting.component';
@@ -23,7 +22,6 @@ export const routes: Routes = [
     { path: 'logout', component: LogoutComponent },
     { path: 'menu-setting', component: MenuSettingComponent, canActivate: [PermissionsService]},
     { path: 'all-articles', component: AllArticlesComponent, canActivate: [PermissionsService]},
-    { path: 'create-article', component: CreateArticleComponent, canActivate: [PermissionsService]},
     { path: 'edit-article/:id/:page', component: EditArticleComponent, canActivate: [PermissionsService]},
     { path: 'disk-units-setting', component: DiskUnitsSettingComponent, canActivate: [PermissionsService]},
     { path: 'unit-files-setting', component: UnitFilesSettingComponent, canActivate: [PermissionsService]},
--- src/app/backoffice/all-articles/all-articles.component.ts
+++ src/app/backoffice/all-articles/all-articles.component.ts
@@ -4,11 +4,15 @@
 
 // Modules
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 import { CommonModule } from '@angular/common';
 import { Router, RouteReuseStrategy } from '@angular/router';
 
 // Model
 import { ArticleCardModel } from '../../models/article-card.model';
+import { TagsModel } from '../../models/tags.model';
+import { CategoriesModel } from '../../models/categories.model';
 
 // Services
 import { CreateArticleService } from '../../services/create-article.service';
@@ -20,7 +24,7 @@ import { ReloadService } from '../../services/reload.service';
 @Component({
   selector: 'app-all-articles',
   standalone: true,
-  imports: [CommonModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './all-articles.component.html',
   styleUrl: './all-articles.component.scss'
 })
@@ -32,6 +36,20 @@ export class AllArticlesComponent implements OnInit {
 
   // Attributes
   dataAllArticles: ArticleCardModel[] = [];
+
+  showForm = false;
+    // Attributes sent to the methode create()
+    title: string = '';                     // Title of the article
+    description: string = '';               // Description of the article
+    checkedTagIds: number[] = [];           // List of tags checked by the user
+    idCategory: string = '';           
+    coverImage: File | null = null;         // Cover image of the article
+
+    // List of tags and categories to display when the component is loaded
+    tagsList: TagsModel[] = [];             
+    CategoriesList: CategoriesModel[] = [];
+      // imageRight: File | null = null;
+  previewImageUrlRight: string | ArrayBuffer | null = null;
 
 
   // Constructor
@@ -55,6 +73,24 @@ export class AllArticlesComponent implements OnInit {
         },
         error: (error) => console.error(error)
       });
+
+      this.createArticleService.getTags().subscribe({
+        next: (data: any) => {
+          this.tagsList = data.body;
+          // console.log(data.body);
+        },
+        error: (error) => console.error(error)
+      });
+
+
+    this.createArticleService.getCategories().subscribe({
+      next: (data: any) => {
+        this.CategoriesList = data.body;
+        // console.log(data.body);
+      },
+      error: (error) => console.error(error)
+    });
+
     });
     this.reload.triggerReload();
   }
@@ -125,5 +161,101 @@ export class AllArticlesComponent implements OnInit {
     });
   }
 
+
+
+
+
+
+
+
+
+
+    /**
+   * 
+   * Method used to check the tags selected by the user
+   * 
+   * @param event 
+   * @param tagId 
+   */
+    onCheckboxChange(event: any, tagId: number): void {
+      if (event.target.checked) {
+        this.checkedTagIds.push(tagId);
+      } else {
+        const index = this.checkedTagIds.indexOf(tagId);
+        if (index > -1) {
+          this.checkedTagIds.splice(index, 1);
+        }
+      }
+      // console.log(this.checkedTagIds);
+    }
+
+
+
+      /**
+   * 
+   * Method used to get the cover image selected by the user
+   * 
+   * @param event 
+   */
+  onFileChange(event: any): void {
+    if (event.target.files && event.target.files.length) {
+      const image = event.target.files[0];
+      this.coverImage = image; // Stocke le fichier sélectionné si nécessaire
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        if (e.target !== null && e.target !== undefined) {
+          this.previewImageUrlRight = e.target.result; // Affecte l'URL de l'aperçu à la propriété
+        } else {
+          console.error('e.target is null or undefined');
+        }
+      };
+      reader.readAsDataURL(image); // Convertit le fichier en URL de données
+    }
+  }
+
+
+
+  /**
+   * Method used to submit the form
+   */
+  submitForm(): void {
+    // Vérifier si idCategory n'est pas vide
+    if (!this.idCategory || this.idCategory.trim() === '') {
+      console.error('Veuillez sélectionner une catégorie avant de soumettre.');
+      return; // Ne pas soumettre le formulaire si idCategory est vide
+    }
+  console.log('this.coverImage', this.coverImage);
+      this.createArticleService.create(this.title, this.description, this.checkedTagIds, this.idCategory, this.coverImage).subscribe({
+        next: (data: any) => {
+          console.log(data);
+          this.reload.triggerReload();
+          this.router.navigate(['/create-article']);
+        },
+        error: (error) => console.error(error)
+      });
+    }
+
+
+    onSelectChange(event: Event): void {
+      const target = event.target as HTMLSelectElement; // Assertion de type
+      const value = target.value;
+      // console.log(value); // Affiche la valeur sélectionnée
+    
+      // Vérifier si la valeur sélectionnée n'est pas vide
+      if (value && value.trim() !== '') {
+        this.idCategory = value; // Affecter la valeur à idCategory
+      } else {
+        console.error('La catégorie sélectionnée est vide.');
+        // Vous pouvez également définir idCategory à une valeur par défaut ou gérer l'erreur comme nécessaire
+      }
+    }
+
+
+      /**
+   * Methode used to toggle the add entrie form
+   */
+  toggleForm() {
+    this.showForm = !this.showForm;
+  }
 
 }
